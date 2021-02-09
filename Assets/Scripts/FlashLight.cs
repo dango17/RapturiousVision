2d84de026db49bac2845cf5a80069b14c1955d60
dango17 <danieloldham75@gmail.com>
2021-02-09 17:04:24 +0000

    Core Mechanics
    Most core machanics finalised

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public GameObject FlashLightObj; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlashLightObj.GetComponent<Light>().enabled = true; 
+    }
+
+    
+    public void TurnOffFlashLight()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            FlashLightObj.GetComponent<Light>().enabled = false;
+        } 
+        else
+        {
+            FlashLightObj.GetComponent<Light>().enabled = true;
+        }
+    }
+}
