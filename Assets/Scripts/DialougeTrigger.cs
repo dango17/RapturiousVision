a3762c7214a5afe9f9dfc2378944aced053acb55
dango17 <danieloldham75@gmail.com>
2021-03-06 02:00:10 +0000

    Revert "Keypads & Friendly Drone Test"
    This reverts commit 9a0e3f811b5475b60b20deba39186309e4d0c6bc.

---
+++ Assets/Scripts/DialougeTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeTrigger : MonoBehaviour
+{
+    public Dialouge dialouge; 
+
+    void OnTriggerStay(Collider player)
+    { 
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            FindObjectOfType<DialogeManager>().StartDialouge(dialouge);
+        }
+        
+    }
+}
