7756dc31d17d6c4a1380eaa8aebc00804d83a34b
dango17 <danieloldham75@gmail.com>
2021-03-18 20:18:05 +0000

    Script Tidy & Keypad start

---
+++ Assets/Scripts/Drones/ChaseTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseTrigger : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider col)
+    {
+        GetComponent<PatrolDrone>().enabled = false;
+        GetComponent<EnemyPathFind>().enabled = true;
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        Destroy(gameObject);
+    }
+}
