f15d42c9f04a684c977494f7b28cc822fc491915
dango17 <danieloldham75@gmail.com>
2021-04-10 22:15:40 +0100

    Light Switch Placement Start
    Light switch placed in room one, if found player can turn on all lights for that room...

--- Assets/Scripts/Mechanics/Lights/LightSwitch.cs
+++ Assets/Scripts/Mechanics/Lights/LightSwitch.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class LightSwitch : MonoBehaviour
 {
     public GameObject light;
+    public Text LightSwitchPrompt; 
+
     private bool on = false;
+
+    public void Start()
+    {
+        //light.SetActive(false);
+    }
 
     void OnTriggerStay(Collider player)
     {
+        LightSwitchPrompt.gameObject.SetActive(true);
+
         if (player.tag == "Player" && Input.GetKeyDown(KeyCode.E) && !on)
         {
             light.SetActive(true);
@@ -20,5 +30,10 @@ public class LightSwitch : MonoBehaviour
             light.SetActive(false);
             on = false;
         }
+    } 
+
+    void OnTriggerExit(Collider player)
+    {
+        LightSwitchPrompt.gameObject.SetActive(false);
     }
 }
