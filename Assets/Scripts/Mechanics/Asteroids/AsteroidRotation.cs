443f15214d619e8f799cd3aa3f612d4c12a11791
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2021-04-18 15:33:26 +0100

    Added rotation script and more text with some fixes to doors
    Added rotation script and more text with some fixes to doors

---
+++ Assets/Scripts/Mechanics/Asteroids/AsteroidRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidRotation : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime * 0, 1, 0));// This script is simply designed to add some motion to the light components for the holders to give the appearance of a power cell
+    }
+}
