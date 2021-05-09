fde3a1443124394d397b205cd4fd7006f61abf42
DevonGritton <55791362+DevonGritton@users.noreply.github.com>
2021-05-09 17:27:17 +0100

    Added Ai core assets
    Added Ai core assets

--- Assets/Scripts/Mechanics/Asteroids/AsteroidRotation.cs
+++ Assets/Scripts/Mechanics/Asteroids/AsteroidRotation.cs
@@ -9,6 +9,6 @@ public class AsteroidRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(Time.deltaTime * 0, 1, 0));// This script is simply designed to add some motion to the light components for the holders to give the appearance of a power cell
+        transform.Rotate(new Vector3(Time.deltaTime * 0, 0, 1));// This script is simply designed to add some motion to the light components for the holders to give the appearance of a power cell
     }
 }
