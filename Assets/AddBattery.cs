9fb27f13a130153a537809665d164b8b236bc3d2
dango17 <danieloldham75@gmail.com>
2021-05-11 00:38:09 +0100

    Battery's & Drone Changes
    Battery system re-written. Dopple Drones to now jumpscare the player. More drones patrol. Navmesh & patrols set up in new areas.

---
+++ Assets/AddBattery.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBattery : MonoBehaviour
+{
+    [Header("This Model")]
+    public GameObject This;
+    [Header("UI-Prompts")]
+    public GameObject PickUpBatteryPrompt;
+    public GameObject BatteryUISlot;
+    [Header("HasBatterBool")]
+    public GameObject BatteryHolder;
+
+    public void Start()
+    {
+        BatteryHolder.GetComponent<BatteryInsert>().HasBattery = false; 
+    }
+
+    public void OnTriggerStay(Collider col)
+    {
+        PickUpBatteryPrompt.SetActive(true);  
+
+        if (col.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                PickUpBatteryPrompt.SetActive(false);
+                BatteryUISlot.SetActive(true); 
+                BatteryHolder.GetComponent<BatteryInsert>().HasBattery = true;
+                This.SetActive(false);             
+            }
+        }
+    } 
+
+    public void OnTriggerExit(Collider col)
+    {
+        PickUpBatteryPrompt.SetActive(false); 
+    }
+}
---
+++ Assets/DoppleDrone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoppleDrone : MonoBehaviour
+{
+    public GameObject EvilDroneUI;
+    
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            EvilDroneUI.SetActive(true);
+            StartCoroutine(LastForSoLong()); 
+        } 
+    } 
+
+    IEnumerator LastForSoLong()
+    {
+        yield return new WaitForSeconds(3);
+        EvilDroneUI.SetActive(false);
+        GetComponent<SphereCollider>().enabled = false; 
+    }
+}
