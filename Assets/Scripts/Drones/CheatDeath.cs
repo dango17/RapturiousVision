134549d1812f303c8ccee4e7513816003213bee5
dango17 <danieloldham75@gmail.com>
2021-05-16 16:41:20 +0100

    More Fixes

--- Assets/Scripts/Drones/CheatDeath.cs
+++ Assets/Scripts/Drones/CheatDeath.cs
@@ -10,4 +10,8 @@ public class CheatDeath : MonoBehaviour
     {
         CheatdeathUI.SetActive(true);
     }
+    public void OnTriggerExit()
+    {
+        CheatdeathUI.SetActive(false);
+    }
 }
