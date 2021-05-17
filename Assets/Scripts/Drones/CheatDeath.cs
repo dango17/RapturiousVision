49ed9ded402f7662d78f5253b51cb91737de638d
dango17 <danieloldham75@gmail.com>
2021-05-17 16:00:08 +0100

    Respawning Added

--- Assets/Scripts/Drones/CheatDeath.cs
+++ Assets/Scripts/Drones/CheatDeath.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheatDeath : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class CheatDeath : MonoBehaviour
     public void OnTriggerStay()
     {
         CheatdeathUI.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("Respawn1");
+        }
     }
     public void OnTriggerExit()
     {
